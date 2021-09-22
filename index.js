8cfe1871428c6a53177a317aa9c16b0277b5eb40
Kevin Digan <kd073@hotmail.com>
2021-09-22 17:13:39 -0400

    commit tree 74ddf5289890b8cee1679b0815263fd57e9d9a2d

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+ // Your code here
+function saturdayFun(fun="roller-skate !") {
+    console.log (`This Saturday, I want to ${fun} `);
+  }
+
+  saturdayFun();
+  //console.log(saturdayFun());
+
+ 
+saturdayFun("bathe my dog !");
+/* console.log(saturdayFun());
+saturdayFun(); 
+console.log(saturdayFun());
+console.log(saturdayFun());
+console.log(saturdayFun()); */  
+
+/* function razzle(lawyer = "Billy", target = "'em") {
+    console.log(`${lawyer} razzle-dazzles ${target}!`);
+  }
+  razzle(); //=> Billy razzle-dazzles 'em!
+  razzle("Methuselah", "T'challah"); //=> Methuselah razzle-dazzles T'challah! */
+
+  /* function saturdayFun(fun = "roller-skate !") {
+    console.log(`This Saturday I wanna ${fun}!`);
+  }
+  saturdayFun();
+  saturdayFun("go to the movies");
+  saturdayFun();
+  (function() {
+    console.log("Yet more razzling");
+  });                                
+
+  const fn = function () {
+    console.log("Yet more razzling");
+  };
+  fn;
+  fn(); */
+
+  function mondayWork(bore = "go to the office !"){
+      console.log(`This Monday, I will ${bore}`);
+  }
+  
+  (mondayWork());
+
+mondayWork("work from home ");
